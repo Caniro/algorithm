f810aee51620115c2d99c2e02b4d0d1bf82d0cd9
Caniro <caniro@naver.com>
2021-01-11 20:55:44 +0900

    20210111 Programmers Level1 - Sum Of Two

---
+++ programmers/Level1/sumOfTwo.cpp
@@ -0,0 +1,72 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+/*
+    정렬 STL 사용법
+*/
+
+using namespace std;
+
+void        swap_by_index(vector<int> &v, int a, int b)
+{
+    int     tmp;
+
+    tmp = v[a];
+    v[a] = v[b];
+    v[b] = tmp;
+}
+
+void        ssort(vector<int> &v)       // selection sort
+{
+    int len = v.size();
+    int min_idx;
+    int min;
+
+    for (int i = 0; i < len - 1; ++i)
+    {
+        min_idx = i;
+        min = v[i];
+        for (int j = i + 1; j < len; ++j)
+        {
+            if (v[min_idx] > v[j])
+            {
+                min_idx = j;
+                min = v[j];
+            }
+        }
+        if (i != min_idx) swap_by_index(v, i, min_idx);
+    }
+}
+
+void        get_answer(vector<int> &numbers, vector<int> &answer)
+{
+    int         len;
+    int         num;
+    int         push_flag;
+
+    len = numbers.size();
+    for (int i = 0; i < len; ++i)
+        for (int j = i + 1; j < len; ++j)
+        {
+            push_flag = true;
+            if (i == j) continue ;      // avoid self-sum
+            num = numbers[i] + numbers[j];
+            for (auto &e: answer)
+                if (e == num)
+                {
+                    push_flag = false;
+                    break ;
+                }
+            if (push_flag) answer.push_back(num);
+        }
+}
+
+vector<int> solution(vector<int> numbers)
+{
+    vector<int> answer;
+
+    get_answer(numbers, answer);
+    ssort(answer);
+    return answer;
+}
